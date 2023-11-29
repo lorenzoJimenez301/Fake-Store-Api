d596c9f0fef649e4e1ae206a47c0e8a1dbbb2561
Lorenzo <lorenzodevelopment301@gmail.com>
2023-11-28 23:12:11 -0600

    empezando axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const[cats, setCats] = useState([]);
+
+  useEffect(()=>{
+    const fetchData = async () =>{
+      try{
+        const response = await axios.get('https://api.thecatapi.com/v1/images/search?limit=10', {
+          params:{
+            breed_id: 'abob',
+          },
+          headers:{
+            'x-api-key': 'live_c2uOZbq1WtOxVcnXrIs7PE8YSd3UqI0Ea4CXsWmPsxkvTqcv6k6AY3OeSbTxCJwv',
+          },
+        });
+        setCats(response.data)
+      }catch (e){
+        console.error(e);
+      }
+    };
+
+    fetchData();
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height:'100vh', display: 'flex', alignItems: 'center', justifyContent:'center'}} className="App">
+      {cats.map((cats) => (
+        <img style={{height: cats.height, width: cats}} key={cats.id} src={cats.url} />
+      ))}
     </div>
   );
 }
