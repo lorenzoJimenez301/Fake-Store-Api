29a04893eceab8289495c5a27c005dc3b717dce6
Lorenzo <lorenzodevelopment301@gmail.com>
2023-11-30 00:39:41 -0600

    se usa la fake store

--- src/App.js
+++ src/App.js
@@ -4,33 +4,30 @@ import axios from 'axios';
 
 function App() {
 
-  const[cats, setCats] = useState([]);
+  const [products, setProducts] = useState([]);
 
-  useEffect(()=>{
-    const fetchData = async () =>{
-      try{
-        const response = await axios.get('https://api.thecatapi.com/v1/images/search?limit=10', {
-          params:{
-            breed_id: 'abob',
-          },
-          headers:{
-            'x-api-key': 'live_c2uOZbq1WtOxVcnXrIs7PE8YSd3UqI0Ea4CXsWmPsxkvTqcv6k6AY3OeSbTxCJwv',
-          },
-        });
-        setCats(response.data)
-      }catch (e){
-        console.error(e);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('https://fakestoreapi.com/products?limit=6');
+        setProducts(response.data);
+      }catch(e){
+        throw e;
       }
-    };
-
-    fetchData();
+    }
+    fetchProducts();
   },[]);
 
   return (
-    <div style={{height:'100vh', display: 'flex', alignItems: 'center', justifyContent:'center'}} className="App">
-      {cats.map((cats) => (
-        <img style={{height: cats.height, width: cats}} key={cats.id} src={cats.url} />
-      ))}
+    <div className="App">
+      <ul className='gridContainer'>
+        {products.map((product)=>(
+          <li key={product.id}>
+            <h2 className='titleProduct'>{product.title}</h2>
+            <img className='imgProduct' src={product.image} alt='Product' />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
